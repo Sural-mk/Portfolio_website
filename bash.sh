d985af2872ba6ee301729e790762aadf65eb17f4
Suralmk <surafelmelaku940@gmail.com>
2024-02-27 15:45:32 +0300

    static

--- bash.sh
+++ bash.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py makemigrations 
 python manage.py migrate
-python manage.py createsuperuser --username Suralmkoracle1313 --password oracle1313##
+python manage.py createsuperuser
