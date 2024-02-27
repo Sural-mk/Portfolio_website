af48b256aeb4fa0382d56379e5bb5862f949f3af
Suralmk <surafelmelaku940@gmail.com>
2024-02-27 22:24:51 +0300

    collect static command

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
