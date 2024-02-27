8f98cfe3d9ad06ed95a1967894e082d745628a06
Suralmk <surafelmelaku940@gmail.com>
2024-02-27 21:24:21 +0300

    collect static command

--- bash.sh
+++ bash.sh
@@ -5,5 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py makemigrations 
 python manage.py migrate
-python manage.py createsuperuser --no-input
 python manage.py collectstatic
