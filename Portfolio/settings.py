5f0f356c254c117487c592338e37ea725f4c5a1f
Suralmk <surafelmelaku940@gmail.com>
2024-02-27 14:46:25 +0300

    static

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR  / 'staticfiles'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
